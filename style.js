da50c8cb4880427832d0433fab97c2dc2b64b317
sarayu-suresh <sarayusuresh888@gmail.com>
2021-05-26 17:50:29 +0530

    final

--- style.js
+++ style.js
@@ -1,1 +1,7 @@
-AOS.init();
+function check() {
+    document.getElementById("demo").innerHTML = "No Slot available right now!";
+}
+
+function check2() {
+    document.getElementById("demo2").innerHTML = "No Slot available right now!";
+}
